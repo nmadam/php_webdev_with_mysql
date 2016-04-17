8a7c163d1bfca1fbef27b7b25ef912e79509823d
nmadam <adam.nancym@gmail.com>
2016-04-17 04:40:49 +0000

    updated captcha file

--- Labs/week13/ch11/captcha.php
+++ Labs/week13/ch11/captcha.php
@@ -1,36 +1,48 @@
 <?php
-  session_start();
+    session_start();
 
-  // Set some important CAPTCHA constants
-  define('CAPTCHA_NUMCHARS', 6);  // number of characters in pass-phrase
-  define('CAPTCHA_WIDTH', 100);   // width of image
-  define('CAPTCHA_HEIGHT', 25);   // height of image
+    // Set some important CAPTCHA constants
+    define('CAPTCHA_NUMCHARS', 6);  // number of characters in pass-phrase
+    define('CAPTCHA_WIDTH', 100);   // width of image
+    define('CAPTCHA_HEIGHT', 25);   // height of image
 
-  // Generate the random pass-phrase
-  $pass_phrase = "";
-  for ($i = 0; $i < CAPTCHA_NUMCHARS; $i++) {    $pass_phrase .= chr(rand(97, 122));  }
+    // Generate the random pass-phrase
+    $pass_phrase = "";
+    for ($i = 0; $i < CAPTCHA_NUMCHARS; $i++) {
+        $pass_phrase .= chr(rand(97, 122));
+    }
 
-  // Store the encrypted pass-phrase in a session variable
-  $_SESSION['pass_phrase'] = SHA($pass_phrase);
+    // Store the encrypted pass-phrase in a session variable
+    $_SESSION['pass_phrase'] = SHA($pass_phrase);
 
-  // Create the image  $img = imagecreatetruecolor(CAPTCHA_WIDTH, CAPTCHA_HEIGHT);  // Set a white background with black text and gray graphics  $bg_color = imagecolorallocate($img, 255, 255, 255);     // white  $text_color = imagecolorallocate($img, 0, 0, 0);         // black  $graphic_color = imagecolorallocate($img, 64, 64, 64);   // dark gray
+    // Create the image
+    $img = imagecreatetruecolor(CAPTCHA_WIDTH, CAPTCHA_HEIGHT);
 
-  // Fill the background
-  imagefilledrectangle($img, 0, 0, CAPTCHA_WIDTH, CAPTCHA_HEIGHT, $bg_color);
-  // Draw some random lines
-  for ($i = 0; $i < 5; $i++) {
-    imageline($img, 0, rand() % CAPTCHA_HEIGHT, CAPTCHA_WIDTH, rand() % CAPTCHA_HEIGHT, $graphic_color);
-  }
+    // Set a white background with black text and gray graphics
+    $bg_color = imagecolorallocate($img, 255, 255, 255);     // white
+    $text_color = imagecolorallocate($img, 0, 0, 0);         // black
+    $graphic_color = imagecolorallocate($img, 64, 64, 64);   // dark gray
 
-  // Sprinkle in some random dots
-  for ($i = 0; $i < 50; $i++) {
-    imagesetpixel($img, rand() % CAPTCHA_WIDTH, rand() % CAPTCHA_HEIGHT, $graphic_color);
-  }
-  // Draw the pass-phrase string
-  imagettftext($img, 18, 0, 5, CAPTCHA_HEIGHT - 5, $text_color, 'Courier New Bold.ttf', $pass_phrase);
+    // Fill the background
+    imagefilledrectangle($img, 0, 0, CAPTCHA_WIDTH, CAPTCHA_HEIGHT, $bg_color);
 
-  // Output the image as a PNG using a header  header("Content-type: image/png");  imagepng($img);
+    // Draw some random lines
+    for ($i = 0; $i < 5; $i++) {
+        imageline($img, 0, rand() % CAPTCHA_HEIGHT, CAPTCHA_WIDTH, rand() % CAPTCHA_HEIGHT, $graphic_color);
+    }
 
-  // Clean up
-  imagedestroy($img);
+    // Sprinkle in some random dots
+    for ($i = 0; $i < 50; $i++) {
+        imagesetpixel($img, rand() % CAPTCHA_WIDTH, rand() % CAPTCHA_HEIGHT, $graphic_color);
+    }
+
+    // Draw the pass-phrase string - copy font onto your server
+    imagettftext($img, 18, 0, 5, CAPTCHA_HEIGHT - 5, $text_color, 'Courier New Bold.ttf', $pass_phrase);
+
+    // Output the image as a PNG using a header
+    header("Content-type: image/png");
+    imagepng($img);
+
+    // Clean up
+    imagedestroy($img);
 ?>
